834830d876bd8cb016f61184b6ff3811931d574d
jaskaran pabla <jpablaguelph@gmail.com>
2024-10-04 13:43:22 -0400

    Add morse code implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,41 @@
 #include <Arduino.h>
 
-void setup() {}
+const float DEFAULT_DELAY = 500; 
+const int LED_PIN = 2; 
 
-void loop() {}
+String morseCode = "-- .. ... ... .. ... ... .. .--. .--. ..";
+
+void sendMorse(String message); // Function declaration
+void lightUp(char character); // Function declaration
+
+void setup() {
+    pinMode(LED_PIN, OUTPUT); 
+}
+
+void loop() {
+    sendMorse(morseCode);
+    delay(10000); 
+}
+
+void sendMorse(String message) {
+    for (char character : message) {
+        if (character == '.') {
+            lightUp('.'); 
+        } else if (character == '-') {
+            lightUp('-');
+        } else if (character == ' ') {
+            delay(3 * DEFAULT_DELAY); 
+        }
+    }
+}
+
+void lightUp(char character) {
+    digitalWrite(LED_PIN, HIGH);
+    if (character == '.') {
+        delay(DEFAULT_DELAY); 
+    } else if (character == '-') {
+        delay(3 * DEFAULT_DELAY); 
+    }
+    digitalWrite(LED_PIN, LOW); 
+    delay(DEFAULT_DELAY); 
+}
