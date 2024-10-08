308848302782fe2ae04de333e0d5ced65899d943
ProfN <91499973+ksampong@users.noreply.github.com>
2024-10-08 22:12:39 +0000

    adding led code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,78 @@
 #include <Arduino.h>
+int led=10;
+int timeunit=500;
 
-void setup() {}
+void setup() {
+  pinMode(led,OUTPUT);
+}
+void loop() {
+  M();
+  I();
+  S();
+  S();
+  I();
+  S();
+  S();
+  I();
+  P();
+  I();
+}
 
-void loop() {}
+
+void M(){
+  digitalWrite(led,HIGH);
+  delay(3*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+   digitalWrite(led,HIGH);
+  delay(3*timeunit);
+  digitalWrite(led,LOW);
+  delay(3*timeunit);
+
+}
+
+void I(){
+   digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+   digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(3*timeunit);
+
+}
+
+void S(){
+   digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+   digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit); 
+  digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(3*timeunit);
+
+}
+void P(){
+    digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+   digitalWrite(led,HIGH);
+  delay(3*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+  digitalWrite(led,HIGH);
+  delay(3*timeunit);
+  digitalWrite(led,LOW);
+  delay(1*timeunit);
+  digitalWrite(led,HIGH);
+  delay(1*timeunit);
+  digitalWrite(led,LOW);
+  delay(3*timeunit);
+}
